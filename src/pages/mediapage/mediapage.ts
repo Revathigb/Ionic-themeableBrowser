191c00065ce9baa5bc8b11ce2bf7cdd9da4662b2
rbommann <rbommann@central.uh.edu>
2017-05-09 11:54:00 -0500

    added the Themeable options in Themable browser function and tested the options in the ios emulator

--- src/pages/mediapage/mediapage.ts
+++ src/pages/mediapage/mediapage.ts
@@ -23,9 +23,38 @@ export class Mediapage {
   }
   openUrl() {
 
-    this.platform.ready().then(() => {
 
-      this.themeableBrowser.create('https://ionic.io', '_blank', {});
+    this.platform.ready().then(() => {
+      const options: ThemeableBrowserOptions = {
+        statusbar: {
+          color: ' #f0f0f0ff'
+        },
+        // toolbar: {
+        //   height: 44,
+        //   color: '#f0f0f0ff'
+        // },
+        title: {
+          color: '#994d00',
+          staticText: ""
+        },
+        closeButton: {
+          image: 'assets/images/close-icon-29.png',
+          imagePressed: 'close_button',
+          align: 'left',
+          event: 'closePressed'
+        },
+
+        transitionstyle: 'fliphorizontal',
+        presentationstyle: 'pagesheet',
+        allowInlineMediaPlayback: 'yes',
+        toolbarposition: 'bottom',
+        closebuttoncaption: 'Return to HomePage'
+      }
+
+
+
+
+      this.themeableBrowser.create('https://ionic.io', '_blank', options);
 
     })
 
